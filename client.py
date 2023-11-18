7c845807a599c669c8b74963ace286b3c33e4b7e
Rhunnan <rhunnandwight.liao.21@usjr.edu.ph>
2023-11-18 22:56:21 +0800

    added bomb functionaliy to check if the user click a bomb

--- client.py
+++ client.py
@@ -4,17 +4,16 @@ import threading
 def betting(Money, betMoney):
     return Money - betMoney
 
-# //pagbuhat rules para sa bumba if maka click og bumba i minus or i lose siya sa iyang ge bet
 
 def receive_messages(client_socket, bet):
     #initialngamatrixscore kay naa diri tnan update sa matrix mabutang
     initialMatrixScore = []
-    #kani diri ang final na score sa user kay ang kwaon ani ang final update ra nga gekan sa initial matrix score
-    matrixScore = []
-    #counter for the multiplyer
+    matrixScore = []  #kani diri ang final na score sa user kay ang kwaon ani ang final update ra nga gekan sa initial matrix score
+    #counter for the multiplyer of bet or earnings
     counter = 0
     #input to stop and calculate your winning
     signalSubmit = "25"
+    #holder for bomb count if there is one then lose
     bombCounter = 0
     #
     earned = 0
@@ -35,19 +34,33 @@ def receive_messages(client_socket, bet):
                             counter +=1
                     earned = bet*counter
                     print("You Won: " + str(earned) )
-                
+                if bombCounter > 0:
+                    print("Alert: You Tap a Bomb!!!\nYou Lose!!!\nBetter Luck Next Time :) :) :)")
+
                 
                 print("User: " + message)
             else:#else if naka abot og 25 then mao ni ang data or message para UI sa terminal
                 for i in range(len(message)):
+                    #storing every updates to initial matrix bomb and score checking
                     initialMatrixScore.append(message[i])
+                #for printing the UI in terminal
                     print("[" +message[i] +']', end= '')
                     if i in {4, 9 ,14 ,19,24}:
                         print()
+                 # to check for boombs in every update
+                  #getting the value from the last 25 index to last one it is the new update
+                matrixScore = initialMatrixScore[-25:]
+                for i in range(len(matrixScore)):
+                   
+                    if matrixScore[i] == "*":
+                        bombCounter +=1
+
+                #if there is bomb then tell the or the user click a bomb then tell the user he/she lose
+                if bombCounter > 0:
+                    print("Alert: You Tap a Bomb!!!\nYou Lose!!!\nBetter Luck Next Time :) :) :)")
 
     except Exception as e:
         print(f"Error receiving messages: {e}")
-
 def main():
     #variables for betting and money of the player
     initialMoney = 500
@@ -74,7 +87,7 @@ def main():
             # Get input from the user and send it to the server
             print("Money: " + str(initialMoney))
             print("Bet: " + str(bet) )
-            message = input()
+            message = input()#for the input of the game
             client_socket.send(message.encode('utf-8'))
 
     except KeyboardInterrupt:
